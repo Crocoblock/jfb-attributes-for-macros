1fab0c760fd26c31ce71f90be1e1852f318d08a3
Sasha Ivanenko <46720998+girafffee@users.noreply.github.com>
2023-08-10 18:34:11 +0300

    FIX: add script only if jfb is activated
    Crocoblock/issues-tracker#3269

--- hooks.php
+++ hooks.php
@@ -6,8 +6,10 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 // Actions
-add_action( 'wp_enqueue_scripts', 'jet_fb_attr_for_macro_register_scripts' );
-add_action( 'jet_plugins/frontend/register_scripts', 'jet_fb_attr_for_macro_register_scripts' );
+if ( function_exists( 'jet_form_builder_init' ) ) {
+	add_action( 'wp_enqueue_scripts', 'jet_fb_attr_for_macro_register_scripts' );
+	add_action( 'jet_plugins/frontend/register_scripts', 'jet_fb_attr_for_macro_register_scripts' );
+}
 
 // Filters
 add_filter( 'jet-form-builder/before-start-form', 'jet_fb_attr_for_macro_before_render_form' );
